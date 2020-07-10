d81dddba8d0dd95c4403be4055161a415fb6f62c
jinho <hahoh0013@gmail.com>
2020-07-10 19:13:44 +0900

    viewsets & routers

--- tutorial_1/snippets/urls.py
+++ tutorial_1/snippets/urls.py
@@ -1,7 +1,51 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+'''
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import renderers
+from .views import SnippetViewSet, UserViewSet, api_root
+
+
+snippet_list = SnippetViewSet.as_view({
+    'get':'list',
+    'post':'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get':'retrieve',
+    'put':'update',
+    'patch':'partial_update',
+    'delete':'destory'
+})
+snippet_highlight = SnippetViewSet.as_view({
+    'get':'highlight'
+}, renderers_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get':'list'
+})
+user_detail = UserViewSet.as_view({
+    'get':'retrieve'
+})
+
+urlpatterns = format_suffix_patterns([
+    path('', api_root),
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippet/<int:pk>/', snippet_detail, name='snippet-detail'),
+    path('snippet/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail'),
+])
+'''
+'''
 urlpatterns = format_suffix_patterns([
     path('', views.api_root),
     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
@@ -10,7 +54,7 @@ urlpatterns = format_suffix_patterns([
     path('users/', views.UserList.as_view(), name='user-list'),
     path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
 ])
-
+'''
 '''
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view()),
--- tutorial_1/snippets/views.py
+++ tutorial_1/snippets/views.py
@@ -4,8 +4,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 
 from rest_framework.parsers import JSONParser
-from rest_framework import status, permissions, renderers
-from rest_framework.decorators import api_view
+from rest_framework import status, permissions, renderers, viewsets
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.reverse import reverse
@@ -18,6 +18,21 @@ from rest_framework import mixins
 from rest_framework import generics
 
 # Create your views here.
+class SnippetViewSet(viewsets.ModelViewSet):
+    # This viewset automatically provides 'list', 'create', 'retrieve', 'update', and 'destroy' action
+    # Addtionally we also provide an extra 'highlight' action
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+'''
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
@@ -32,7 +47,13 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,]
+'''
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    # This viewset automatically provides 'list' and 'detail' actions
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
+'''
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -40,7 +61,7 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
+'''
 @api_view(['GET'])
 def api_root(request, format=None):
     return Response({
